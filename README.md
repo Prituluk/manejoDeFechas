e237f4a4dfd9e65eba4b1d204713bd1a5c172872
Guillermo Prituluk <guillermoprituluk@gmail.com>
2025-09-26 10:14:25 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Proyect PrituDate
+
+Una utilidad para manejar fechas en formato timestamp y long time.
+
+#Install
+
+```bash
+npm install pritudate
+```
+
---
+++ src/index.js
@@ -0,0 +1,22 @@
+function getTimestamp() {
+  return Date.now();
+}
+
+function getLongTime(locale = 'es-ES') {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    seconds: 'numeric',
+    timeZoneName: 'short'
+  }
+  return new Date().toLocaleDateString(locale, options);
+}
+
+module.exports = {
+  getTimestamp,
+  getLongTime
+}
